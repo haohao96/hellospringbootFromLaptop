183e82dfd17c49b9dd40fb0ee32a77e89b52921f
xrzs <18437927639@163.com>
2021-07-28 15:50:04 +0800

    add mvc config

---
+++ src/main/java/com/pyh/hellospringboot/config/MvcConfig.java
@@ -0,0 +1,17 @@
+package com.pyh.hellospringboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//MVC的配置类   关于mvc的配置都可以写在这里，但要实现WebMvcConfigurer
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+//    为url设置跳转页面，这样就不用写controller了
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index").setViewName("index");
+        registry.addViewController("/index.html").setViewName("index");
+    }
+}
